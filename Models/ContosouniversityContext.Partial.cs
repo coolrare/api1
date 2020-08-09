803da8d8c47b7912336f1e31a743e8db93b26445
Will <doggy.huang@gmail.com>
2020-08-10 07:31:43 +0800

    示範如何擴充 ContosouniversityContext 部分類別 (partial class)

---
+++ Models/ContosouniversityContext.Partial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace api1.Models
+{
+    public partial class ContosouniversityContext : DbContext
+    {
+        public override int SaveChanges()
+        {
+            // TODO
+
+            return base.SaveChanges();
+        }
+    }
+}
